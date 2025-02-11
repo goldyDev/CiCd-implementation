e27ace23df84a42f950966c7ae854d33d31c1c72
Goldy Singh <goldy.singh@kissan.ai>
2025-02-11 15:03:52 +0530

    updated tailwind v4 new ersion with flowbite

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -1,6 +1,6 @@
 import { Button } from 'flowbite-react'
 import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const HomePage: React.FC = () => {
   const navigate=useNavigate()
